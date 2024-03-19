6590bf6dd87600a1877fade4b5c1a74159de33dc
Eugentib <eugen.tib@gmail.com>
2024-03-19 20:08:31 +0200

    Corectare conditii pentru alerta gr2

--- db.js
+++ db.js
@@ -17,7 +17,7 @@ async function query (sql, params) {
 }
 
 async function functiePeriodica () {
-  console.log('Funcție asincronă apelată periodic.')
+//  console.log('Funcție asincronă apelată periodic.')
 
   try {
     const rezultat = await alerta_gr2()
@@ -54,7 +54,7 @@ async function alerta_gr2 () {
   GROUP BY mac
   HAVING COUNT(eroare) >= ?;`
 
-  const mac_list1 = await query(sql, [conditii[0].val, conditii[1].val])
+  const mac_list1 = await query(sql, conditii[1].val, [conditii[0].val])
 
   sql = `SELECT mac FROM ( 
     SELECT mac, SUM(TIME_TO_SEC(durata)) AS total_durata 
@@ -71,7 +71,7 @@ async function alerta_gr2 () {
   ) AS subquery 
   WHERE total_durata >= ? * 60 * 60;`
 
-  const mac_list2 = await query(sql, [conditii[2].val, conditii[3].val])
+  const mac_list2 = await query(sql, conditii[3].val, [conditii[2].val])
 
   if (mac_list1.length + mac_list2.length == 0) {
     return 'Nu este nevoie de alerta.'
